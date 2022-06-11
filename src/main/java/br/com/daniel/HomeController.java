d7805e88e40f9df70a2df4c0e4d7caf5674c78d8
Douglas Fernandes <douglasf.filho@gmail.com>
2022-06-11 16:48:36 -0300

    fix: file format

--- src/main/java/br/com/daniel/HomeController.java
+++ src/main/java/br/com/daniel/HomeController.java
@@ -5,7 +5,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller("/")
 public class HomeController {
-    private static final long serialVersionUID = 1L;
 
     @GetMapping
     public String home() {
