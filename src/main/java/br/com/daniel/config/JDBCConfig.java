ecf5d7cfc85b72c5476b72eaa3a805d36406f394
Douglas Fernandes <douglasf.filho@gmail.com>
2022-06-16 23:28:47 -0300

    fixing: update embbeded deployment must add log4j

--- src/main/java/br/com/daniel/config/JDBCConfig.java
+++ src/main/java/br/com/daniel/config/JDBCConfig.java
@@ -1,20 +1,13 @@
 package br.com.daniel.config;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
-import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
-
-import javax.sql.DataSource;
-
-@Configuration
+//@Configuration
 public class JDBCConfig {
-    @Bean
-    public DataSource dataSource() {
-        return new EmbeddedDatabaseBuilder()
-                .setType(EmbeddedDatabaseType.H2)
-                .addScript("classpath:jdbc/schema.sql")
-                .addScript("classpath:jdbc/test-data.sql")
-                .build();
-    }
+//    @Bean
+//    public DataSource dataSource() {
+//        return new EmbeddedDatabaseBuilder()
+//                .setType(EmbeddedDatabaseType.H2)
+//                .addScript("classpath:jdbc/schema.sql")
+//                .addScript("classpath:jdbc/test-data.sql")
+//                .build();
+//    }
 }
