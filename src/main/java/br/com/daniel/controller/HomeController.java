93bfe87eaf07474d2e1a4145e7821e3531a0e516
Douglas Fernandes <douglasf.filho@gmail.com>
2022-07-02 12:20:36 -0300

    refactor: created a test giving access controller

--- src/main/java/br/com/daniel/controller/HomeController.java
+++ src/main/java/br/com/daniel/controller/HomeController.java
@@ -1,20 +1,13 @@
 package br.com.daniel.controller;
 
-import br.com.daniel.annotations.Unsecured;
-import br.com.daniel.security.service.UserService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
-@Controller
+//@Controller
 public class HomeController {
-    @Autowired
-    UserService userService;
 
     @GetMapping
-    @Unsecured
     public String home() {
-        this.userService.login("admin", "YWRtaW4=");
         return "index";
     }
 
